cb48781d9d7785bf0a105a56fce9d197d35aa327
Artem <artem.k@outlook.com>
2023-12-07 12:13:23 -0500

    newubuntu bat

--- newdevice/newubuntu.sh
+++ newdevice/newubuntu.sh
@@ -4,7 +4,7 @@ echo 'ubuntu app upload begins'
 
 #apt update && apt upgrade
 
-apps=(curl wget openssh snap mc tldr tmux fzf)
+apps=(curl wget openssh snap mc tldr tmux fzf bat)
 
 file='./updateResults'
 > $file #make it empty
