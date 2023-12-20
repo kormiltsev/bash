bbe4a5c61a2cd4aeb0d95449e5edd02d9e3ea01d
Artem <artem.k@outlook.com>
2023-12-20 12:13:23 -0500

    newubuntu rsync

--- newdevice/newubuntu.sh
+++ newdevice/newubuntu.sh
@@ -4,7 +4,7 @@ echo 'ubuntu app upload begins'
 
 #apt update && apt upgrade
 
-apps=(curl wget openssh snap mc tldr tmux fzf bat feh neofetch)
+apps=(curl wget openssh snap mc tldr tmux fzf bat feh neofetch rsync)
 
 file='./updateResults'
 > $file #make it empty
