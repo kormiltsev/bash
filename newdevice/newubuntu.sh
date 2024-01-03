73224cb5f6dfe6f671ae7253c9aa7bb709f67094
Artem <artem.k@outlook.com>
2024-01-03 12:13:23 -0500

    newubuntu gdu

--- newdevice/newubuntu.sh
+++ newdevice/newubuntu.sh
@@ -4,7 +4,7 @@ echo 'ubuntu app upload begins'
 
 #apt update && apt upgrade
 
-apps=(curl wget openssh snap mc tldr tmux fzf bat feh neofetch rsync w3m tty-clock nano micro)
+apps=(curl wget openssh snap mc tldr tmux fzf bat feh neofetch rsync w3m tty-clock nano micro gdu)
 
 file='./updateResults'
 > $file #make it empty
